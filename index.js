2864fd94b332658f261490d1c240a818a1b12693
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-21 17:15:05 +0000

    virtual DOM

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const input = document.querySelector(`#idInputText`);
 //console.log(input.value);
 const sectionOne = document.getElementById(`idSectionMain`);
 
-//*TODO ===  === === === === === === === === === === === */
-// ==> Create Virtual DOM Images ===
+//*TODO ===  === === === Create Virtual DOM === === === === === === === === */
 const image = document.createElement(`img`);
 image.src =
   "https://images.unsplash.com/photo-1663720409027-5f7b019b0990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80";
