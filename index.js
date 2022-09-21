d1fbb1ffa241e412d9696a67acf204eb5fc205d7
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-21 17:57:50 +0000

    manipulation of DOM

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const textOne = document.querySelector(`#idTextOne`);
 const textTwo = document.querySelector(`#idTextTwo`);
 const textThree = document.querySelector(`#idTextThree`);
 const input = document.querySelector(`#idInputText`);
-//console.log(input.value);
 const sectionOne = document.getElementById(`idSectionMain`);
 
 //*TODO ===  === === === Create Virtual DOM === === === === === === === === */
+// getAttribute;
+// setAttribute;
+// classList => .add / .remove / .toggle / .contains ===
+// .append
 const image = document.createElement(`img`);
 image.src =
   "https://images.unsplash.com/photo-1663720409027-5f7b019b0990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80";
+//image.setAttribute(`src`, `./url`);
 image.width = 250;
 image.height = 200;
+//image.setAttribute(`class`, `img-DOM`);
+image.classList.add("img-DOM");
 sectionOne.appendChild(image);
 
 title.innerHTML = "Manipulation Of DOM";
 textOne.textContent = "Javascript Manipulation Text";
 textTwo.textContent = "This is Text Two";
 textThree.textContent = "Three Text New";
+input.value = 150;
