70256e5afa6db8d5b722c30972159124d01d73cc
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-21 16:35:29 +0000

    working js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //** === Javascript Practice Challenge === */
+//*TODO === Variables  === === === === === === ===  */
+const title = document.querySelector(`#mainLayout`);
+const textOne = document.querySelector(`#idTextOne`);
+const textTwo = document.querySelector(`#idTextTwo`);
+const textThree = document.querySelector(`#idTextThree`);
+const input = document.querySelector(`#idInputText`);
+
+//*TODO ===  === === === === === === === === === === === */
+title.innerHTML = "Manipulation Of DOM";
