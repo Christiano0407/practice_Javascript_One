75d3092f408f9f0bf1372ea445029cfaa5811f2b
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-22 14:18:40 +0000

    caption new

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const idImage = document.querySelector(`#idImage`);
 
 //*TODO ===  === === === Create Virtual DOM === === === === === === === === */
 const image = document.createElement(`img`);
+const figureCaption = document.createElement(`figcaption`);
 image.src =
   "https://images.unsplash.com/photo-1663720409027-5f7b019b0990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80";
 //image.setAttribute(`src`, `./url`);
@@ -17,6 +18,8 @@ image.width = 250;
 image.height = 200;
 //image.setAttribute(`class`, `img-DOM`);
 image.classList.add("img-DOM");
+image.alt = "imgNew";
+figureCaption.textContent = "Image App New";
 title.innerHTML = "Manipulation Of DOM";
 textOne.textContent = "APP Images";
 
@@ -26,5 +29,5 @@ btn.addEventListener("click", eventClick);
 function eventClick() {
   console.log(input.value);
   console.log(inputName.value);
-  idImage.appendChild(image);
+  idImage.append(image, figureCaption);
 }
