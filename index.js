939fdd61f37a2a33af1cbd53605053266fe5b32d
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-22 13:42:27 +0000

    App images

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
 //*TODO === Variables  === === === === === === ===  */
 const title = document.querySelector(`#idTitle`);
 const textOne = document.querySelector(`#idTextOne`);
-const textTwo = document.querySelector(`#idTextTwo`);
-const textThree = document.querySelector(`#idTextThree`);
 const input = document.querySelector(`#idInputText`);
 const sectionOne = document.getElementById(`idSectionMain`);
+const btn = document.getElementById(`idBtnClick`);
 
 //*TODO ===  === === === Create Virtual DOM === === === === === === === === */
-// getAttribute;
-// setAttribute;
-// classList => .add / .remove / .toggle / .contains ===
-// .append
 const image = document.createElement(`img`);
 image.src =
   "https://images.unsplash.com/photo-1663720409027-5f7b019b0990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80";
@@ -23,7 +18,11 @@ image.classList.add("img-DOM");
 sectionOne.appendChild(image);
 
 title.innerHTML = "Manipulation Of DOM";
-textOne.textContent = "Javascript Manipulation Text";
-textTwo.textContent = "This is Text Two";
-textThree.textContent = "Three Text New";
-console.log(input.value);
+textOne.textContent = "APP Images";
+
+//*Todo === Events === === === */
+btn.addEventListener("click", eventClick);
+
+function eventClick() {
+  console.log(input.value);
+}
