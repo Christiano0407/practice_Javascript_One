8fad7a2095e9085af02b3c87c93b7a213850d132
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-02 15:04:59 +0000

    product-detals add javascript

--- public/mobile/appShop.js
+++ public/mobile/appShop.js
@@ -4,6 +4,8 @@ const userIcon = document.querySelector(`#idNavUser`);
 const desktopMenu = document.querySelector("#idDesktopMenu");
 const burgerMenu = document.querySelector("#idMenuBurger");
 const menuMobile = document.querySelector("#idMobileMenu");
+const shoppingCart = document.querySelector(`#idShoppingCart`);
+const productDetail = document.querySelector(`#idProductDetail`);
 
 //** === === === === === Execute Code ===  === === === === */
 //desktop.classList.add("visible");
@@ -25,3 +27,13 @@ const mobileMenu = () => {
   }
 };
 burgerMenu.addEventListener("click", mobileMenu);
+
+//*? === Product Detail ===  */
+// productDetail.classList.contains("inactive"
+const onShopping = () => {
+  if (productDetail) {
+    productDetail.classList.toggle("inactive");
+  }
+};
+
+shoppingCart.addEventListener("click", onShopping);
